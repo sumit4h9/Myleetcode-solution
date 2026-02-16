4a2c3ec02f8a2918df2ab9182d16a7bd2e6c1891
Sumit Sunil Shelar <87257778+sumit4h9@users.noreply.github.com>
2026-02-16 19:33:45 +0530

    2 approach

--- linkedList/Reverse-Linked-List.java
+++ linkedList/Reverse-Linked-List.java
@@ -1,24 +1,38 @@
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11// class Solution {
-12//     public ListNode reverseList(ListNode head) {
-13//         if (head == null || head.next == null) {
-14//             return head;
-15//         }
-16//         ListNode newHead = reverseList(head.next);
-17        
-18//         head.next.next = head;
-19//         head.next = null;
-20
-21//         return newHead;
-22//     }
-23// }
-24
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+//         ListNode newHead = reverseList(head.next);
+        
+//         head.next.next = head;
+//         head.next = null;
+
+//         return newHead;
+//     }
+// }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}

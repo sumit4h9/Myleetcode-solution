61cdc0986d69fffb144dcf588f4b8fda953d8ec7
Sumit Sunil Shelar <87257778+sumit4h9@users.noreply.github.com>
2026-02-16 22:59:41 +0530

    [141] [Time Beats: 100.00%] [Memory Beats: 52.10%] - LeetPush

---
+++ linkedList/Linked-List-Cycle.java
@@ -0,0 +1,29 @@
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public boolean hasCycle(ListNode head) {
+14        ListNode slow = head;
+15        ListNode fast = head;
+16
+17
+18        while(fast!=null && fast.next!=null){
+19            slow = slow.next;
+20            fast = fast.next.next;
+21
+22            if(slow==fast){
+23                return true;
+24            }
+25        }
+26        return false;
+27
+28    }
+29}

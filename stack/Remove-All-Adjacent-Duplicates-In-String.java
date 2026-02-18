03fe0aeb0a72fcacfd44e335adbbcf6e7b80a9af
Sumit Sunil Shelar <87257778+sumit4h9@users.noreply.github.com>
2026-02-18 20:23:56 +0530

    [1047] [Time Beats: 61.87%] [Memory Beats: 38.46%] - LeetPush

---
+++ stack/Remove-All-Adjacent-Duplicates-In-String.java
@@ -0,0 +1,19 @@
+1class Solution {
+2    public String removeDuplicates(String s) {
+3        Stack<Character> st = new Stack<>();
+4
+5        for(char c:s.toCharArray()){
+6            if(!st.isEmpty() && st.peek() == c){
+7                st.pop();
+8            }
+9            else{
+10                st.push(c);
+11            }
+12        } 
+13        StringBuilder sb = new StringBuilder();
+14        for (char c : st) {
+15            sb.append(c);
+16        }
+17        return sb.toString();
+18    }
+19}

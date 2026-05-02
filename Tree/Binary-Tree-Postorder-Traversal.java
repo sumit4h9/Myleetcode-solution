3e6cd2caddd8134e7a05d3fa763f788dd6d3e350
Sumit Sunil Shelar <87257778+sumit4h9@users.noreply.github.com>
2026-05-02 18:31:48 +0530

    [145] [Time Beats: 100.00%] [Memory Beats: 14.51%] - LeetPush

---
+++ Tree/Binary-Tree-Postorder-Traversal.java
@@ -0,0 +1,30 @@
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public List<Integer> postorderTraversal(TreeNode root) {
+18        List<Integer> res = new ArrayList<>();
+19        helper(root,res);
+20        return res;
+21    }
+22    private void helper(TreeNode root, List<Integer> res){
+23        if(root ==null){
+24            return ;
+25        }
+26        helper(root.left,res);
+27        helper(root.right,res);
+28        res.add(root.val);
+29    }
+30}
